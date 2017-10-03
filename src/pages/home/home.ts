ae5865f890fdaf49cffcc3e771adbfa2f9dd0d72
James West <judgewest2000@hotmail.co.uk>
2017-10-03 23:27:29 +0100

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NavController, Loading, LoadingController } from 'ionic-angular';
+
+import Tesseract from 'tesseract.js';
+import { FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,46 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('testimage') testImage: ElementRef;
 
+  processedText = new FormControl('');
+
+  constructor(public navCtrl: NavController, private loadingCtrl: LoadingController) {
+
+  }
+
+  loading = (() => {
+    let loadMessage: Loading;
+
+    return {
+      turnOn: () => {
+        loadMessage = this.loadingCtrl.create({
+          content: 'Please Wait, doing something awesome'
+        });
+        loadMessage.present();
+      },
+      turnOff: () => loadMessage.dismiss()
+    };
+
+  })();
+
+  process() {
+
+    let loading = this.loading;
+
+    loading.turnOn();
+
+    const element = this.testImage.nativeElement as HTMLImageElement;
+
+    let processedText = this.processedText;
+
+
+    Tesseract.recognize(element)
+      .then(function (result: { text: string }) {
+        processedText.setValue(result.text);
+        console.log(result)
+        loading.turnOff();
+      })
   }
 
 }
